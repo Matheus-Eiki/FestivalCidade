f1b48e212b5b09249992da785f6549f988cdbaed
Matheus <m.eiki.ikeda@gmail.com>
2026-08-25 15:47:39 -0300

    chore: Criando o sistema aonde o cliente recebe as perguntas e pode responder

--- FestivalCidade/Program.cs
+++ FestivalCidade/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using FestivalCidade;
+
+Console.WriteLine("****************Sistema de Ingressos Festival Cidade****************");
+Console.WriteLine("");
+
+Sistem sistema = new Sistema;
+
+Console.Write("Digite o seu nome: ");
+sistema.NomeFa = Console.ReadLine();
+
+Console.Write("Digite a quantidade de dias que deseja ir ao festival: ");
+sistema.Dias = int.Parse(Console.ReadLine());
+
+Console.Write("Digite quantos pontos de fidelidade você tem: ");
+sistema.PontosFidelidade = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o cupom de desconto (se houver): ");
+sistema.CupomDesconto = Console.ReadLine();
+
+
